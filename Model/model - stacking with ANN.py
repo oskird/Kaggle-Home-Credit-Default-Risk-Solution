86b3a5bfdfa081d71dd7b14af3cf23fb74e48c65
Shuyu Zhou <41087309+oskird@users.noreply.github.com>
2019-01-22 12:23:14 +0800

    Update model - stacking with ANN.py

--- Model/model - stacking with ANN.py
+++ Model/model - stacking with ANN.py
@@ -4,8 +4,10 @@ from keras.models import Sequential
 from keras.optimizers import Adam
 from keras.callbacks import EarlyStopping
 
+# should import oof_train and oof_test dateset
+
 model = Sequential()
-model.add(Dense(8, input_shape=(X_train.shape[1],)))
+model.add(Dense(8, input_shape=(oof_train.shape[1],)))
 model.add(Activation('relu'))
 model.add(Dense(7))
 model.add(Activation('relu'))
@@ -23,5 +25,5 @@ model.add(Activation('sigmoid'))
 my_optimizer = Adam(lr=0.01, beta_1=0.9, beta_2=0.999, epsilon=1e-08)
 model.compile(optimizer=my_optimizer, loss='binary_crossentropy')
 early_stopping_monitor = EarlyStopping(patience=6)
-model.fit(X_train, y, validation_split=0.2, epochs=50, batch_size=16392, callbacks=[early_stopping_monitor])
-y_pred = model.predict(X_train)
+model.fit(oof_train, y, validation_split=0.2, epochs=50, batch_size=16392, callbacks=[early_stopping_monitor])
+y_pred = model.predict(oof_test)
